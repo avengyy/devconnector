d6a6ad5e89de115102d0d2614fb42de483319e7f
Zachary Zhou <avengyy@gmail.com>
2018-10-05 16:14:37 +0800

    Complete Profile API Routes;

--- models/Profile.js
+++ models/Profile.js
@@ -82,10 +82,6 @@ const ProfileSchema = new Schema({
         type: String,
         required: true
       },
-      company: {
-        type: String,
-        required: true
-      },
       from: {
         type: Date,
         required: true
