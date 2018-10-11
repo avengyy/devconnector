6c51c236fb5fbef2cfa1358fd84b1ea0c9f972d9
Zachary Zhou <avengyy@gmail.com>
2018-10-11 16:19:38 +0800

    Added layout and events for  login and register pages;

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      errors: {}
+    };
+
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+    this.onSubmitHandler = this.onSubmitHandler.bind(this);
+  }
+
+  onChangeHandler(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  onSubmitHandler(event) {
+    event.preventDefault();
+
+    // Destructure Component State
+    const { email, password } = this.state;
+
+    // Assign state properties to user object
+    const user = {
+      email,
+      password
+    };
+
+    console.log(user);
+  }
+
+  render() {
+    return (
+      <div className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Log In</h1>
+              <p className="lead text-center">
+                Sign in to your DevConnector account
+              </p>
+              <form onSubmit={this.onSubmitHandler}>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email Address"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.onChangeHandler}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChangeHandler}
+                  />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
