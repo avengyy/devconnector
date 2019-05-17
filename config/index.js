48857600eb480a9f67dc838d89076c01209e6285
Zachary Zhou <avengyy@gmail.com>
2019-05-17 15:24:21 +0800

    feat(v2): add sign up api

--- config/index.js
+++ config/index.js
@@ -3,5 +3,6 @@ require('dotenv').config();
 module.exports = {
   mongoURI: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${
     process.env.DB_HOST
-  }/test?retryWrites=true`
+  }/test?retryWrites=true`,
+  jwtSecret: process.env.JWT_SECRET
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { check, validationResult } = require('express-validator/check');
 const { sendSuccess, sendFailure } = require('../utils');
+const { jwtSecret } = require('../../config');
 
 const User = require('../../models/User');
 
@@ -29,17 +33,48 @@ router.post(
       return sendFailure(errors.array(), res);
     }
 
-    // 1. Check if user exist
     const { name, email, password } = req.body;
 
     try {
-      await User.findOne();
+      // 1. Check if user exist
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return sendFailure([{ msg: 'User already exists' }], res);
+      }
+
       // 2. Get user avatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
 
       // 3. Hash password
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
 
       // 4. Return access token
-      sendSuccess('User OK', res);
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(payload, jwtToken, { expiresIn: 3600 * 100 }, (err, token) => {
+        if (err) throw err;
+
+        return sendSuccess({ token }, res);
+      });
     } catch (error) {
       next(error);
     }
