b18011b60f2723c76091639e167906b6e2a4238e
Zachary Zhou <avengyy@gmail.com>
2019-06-12 12:13:23 +0800

    feat(v2): add get current user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const { auth } = require('../../middleware');
+const { sendSuccess, sendFailure } = require('../utils');
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 /**
- * @route GET api/profile
- * @description Test Route
- * @access Public
+ * @route GET api/profile/me
+ * @description Get current user profile
+ * @access Private
  */
-router.get('/', (req, res) => {
-  res.send('Profile OK');
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+
+    if (!profile) {
+      return sendFailure(res, { msg: 'There is no profile for this user' });
+    }
+
+    sendSuccess(res, profile);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
