24253bdf81cf654b0e45b4ec8633d5ffce384560
Zachary Zhou <avengyy@gmail.com>
2019-07-08 12:49:44 +0800

    feat(v2): add new education and delete education routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -265,4 +265,93 @@ router.delete('/experience/:exp_id', auth, async (req, res, next) => {
   }
 });
 
+/**
+ * @route PUT api/profile/education
+ * @description Add profile education
+ * @access Private
+ */
+router.put(
+  '/education',
+  [
+    auth,
+    [
+      check('school', 'School is required')
+        .not()
+        .isEmpty(),
+      check('degree', 'Degree is required')
+        .not()
+        .isEmpty(),
+      check('fieldofstudy', 'Field of study is required')
+        .not()
+        .isEmpty(),
+      check('from', 'From date is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return sendFailure(res, errors.array());
+    }
+
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    const newEdu = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.education.unshift(newEdu);
+
+      await profile.save();
+
+      sendSuccess(res, profile);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+/**
+ * @route DELETE api/profile/education/:edu_id
+ * @description Delete profile education
+ * @access Private
+ */
+router.delete('/education/:edu_id', auth, async (req, res, next) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.education
+      .map(item => item.id)
+      .indexOf(req.params.edu_id);
+
+    profile.education.splice(removeIndex, 1);
+
+    await profile.save();
+
+    sendSuccess(res, profile);
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
