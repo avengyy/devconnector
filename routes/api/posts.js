14390f250ff70fda488a2d063c24aac16d8588cd
Zachary Zhou <avengyy@gmail.com>
2019-07-09 10:13:11 +0800

    feat(v2): add new user post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,13 +1,52 @@
 const express = require('express');
 const router = express.Router();
+const { auth } = require('../../middleware');
+const { sendSuccess, sendFailure } = require('../utils');
+const { check, validationResult } = require('express-validator/check');
+
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 /**
- * @route GET api/posts
- * @description Test Route
- * @access Public
+ * @route POST api/posts
+ * @description Create a post
+ * @access Private
  */
-router.get('/', (req, res) => {
-  res.send('Posts OK');
-});
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return sendFailure(res, errors.array());
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
+
+      const post = await newPost.save();
+
+      sendSuccess(res, post);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 module.exports = router;
